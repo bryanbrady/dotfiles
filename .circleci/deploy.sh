e4047fca90cc9377c071afce027442a4980d8ef1
Bryan Brady <bryan@swift-nav.com>
2019-03-20 22:23:21 -0700

    add circle

---
+++ .circleci/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sha=`git rev-parse HEAD`
+sh gen.sh > /tmp/env
+git config --global user.email "bryan.brady@gmail.com"
+git config --global user.name "bryan brady"
+git clone git@github.com:bryanbrady/asdf-sh.git
+cd asdf-sh
+cp /tmp/env .
+if ! git diff --exit-code; then
+  git add -u
+  git commit -m "update env for $sha"
+  git push origin master
+fi
