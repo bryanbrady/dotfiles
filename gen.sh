1496ba1bea5038dd9ba05763ae9963a7cc4b4949
Bryan Brady <bryan@swift-nav.com>
2019-03-20 22:20:36 -0700

    update gen.sh to run in alpine

--- gen.sh
+++ gen.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
+cat \
+  .bash_include \
+  .bash_colors \
+  .bash_dircolors \
+  .bash_alias \
+  .bash_functions \
 
-THIS_DIR=$(cd $(dirname $0); pwd -P)
-FILES=(
-  ${THIS_DIR}/.bash_include    \
-  ${THIS_DIR}/.bash_colors     \
-  ${THIS_DIR}/.bash_dircolors  \
-  ${THIS_DIR}/.bash_alias      \
-  ${THIS_DIR}/.bash_functions  \
-  )
-
-for f in ${FILES[@]}; do
-  cat $f
-done
