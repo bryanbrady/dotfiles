bd48ffd21872882f5627c9308f055f56d1512d34
Bryan Brady <bryan.brady@gmail.com>
2019-03-29 17:47:36 -0700

    update gen.sh script for dot/

--- gen.sh
+++ gen.sh
@@ -1,11 +1,11 @@
 
 echo "go () {"
 cat \
-  .bash_include \
-  .bash_colors \
-  .bash_dircolors \
-  .bash_alias \
-  .bash_functions
+  dot/.bash_include \
+  dot/.bash_colors \
+  dot/.bash_dircolors \
+  dot/.bash_alias \
+  dot/.bash_functions
 echo "}"
 echo go
 
